977f2c0ca9ccf8e537c46beea93d44abb64a4a0d
Alif024 <aleefrock12345@gmail.com>
2024-01-24 11:51:15 +0700

    #

--- server.mjs
+++ server.mjs
@@ -1,41 +1,42 @@
-import express from 'express'
-import mysql from 'mysql2'
-import cors from 'cors'
+import express from "express";
+import mysql from "mysql2";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 const con = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'coffee_db'
-})
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "coffee_db",
+});
 
 con.connect((err) => {
   if (err) {
     console.log(err);
     return;
   }
-  console.log('Success Connect');
-})
+  console.log("Success Connect");
+});
 
-app.use('/insert', cors(), (req, res) => {
+app.use("/insert", cors(), (req, res) => {
   const { username, password, email } = req.body;
-  const sql = 'INSERT INTO customers (username, password, email) VALUES (?, ?, ?)';
+  const sql =
+    "INSERT INTO customers (username, password, email) VALUES (?, ?, ?)";
   con.query(sql, [username, password, email], (error) => {
     if (error) {
-      console.error('Error while inserting data:', error);
-      return res.status(500).send('Error while inserting data');
+      console.error("Error while inserting data:", error);
+      return res.status(500).send("Error while inserting data");
     }
-    res.status(200).send('Data inserted successfully');
+    res.status(200).send("Data inserted successfully");
   });
 });
 
 app.post("/read", async (req, res) => {
   const { username, password } = req.body;
-  const sql = 'SELECT * FROM customers WHERE `username` = ? AND `password` = ?';
+  const sql = "SELECT * FROM customers WHERE `username` = ? AND `password` = ?";
   try {
     con.query(sql, [username, password], (err, result) => {
       if (err) {
@@ -43,11 +44,28 @@ app.post("/read", async (req, res) => {
         return res.status(400).send();
       }
       res.status(200).json(result);
-    })
+    });
   } catch (err) {
     console.log(err);
     return res.status(500).send();
   }
 });
 
-app.listen(5000, () => console.log('Server is running'));
+app.use("/insertDataAccount", cors(), (req, res) => {
+  const { username, firstname, lastname, telephone, address } = req.body;
+  const sql =
+    "UPDATE `customers` SET `telephone`=?,`address`=?,`firstname`=?,`lastname`=? WHERE `username` = ?";
+  con.query(
+    sql,
+    [telephone, address, firstname, lastname, username],
+    (error) => {
+      if (error) {
+        console.error("Error while inserting data:", error);
+        return res.status(500).send("Error while inserting data");
+      }
+      res.status(200).send("Data inserted successfully");
+    }
+  );
+});
+
+app.listen(5000, () => console.log("Server is running"));
--- src/SettingsContent.jsx
+++ src/SettingsContent.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
-import Guess from './assets/user.png'
-import './SettingsContent.css'
+import Axios from "axios";
+import Guess from "./assets/user.png";
+import "./SettingsContent.css";
 
 // eslint-disable-next-line react/prop-types, no-unused-vars
 const SettingsContent = ({ dataUser, setDataUser }) => {
@@ -8,12 +9,78 @@ const SettingsContent = ({ dataUser, setDataUser }) => {
   //   console.log(dataUser);
   // }
 
-  return (
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // ป้องกันการส่งฟอร์มไปยังหน้าอื่น
 
+    // ดึงค่าจากเขตข้อมูลแต่ละช่องได้ดังนี้
+    const firstName = document.querySelector('input[name="firstName"]').value;
+    const lastName = document.querySelector('input[name="lastName"]').value;
+    const tel = document.querySelector('input[name="tel"]').value;
+    const NoAddr = document.querySelector('input[name="NoAddr"]').value;
+    const subdistrict = document.querySelector(
+      'input[name="subdistrict"]'
+    ).value;
+    const district = document.querySelector('input[name="district"]').value;
+    const city = document.querySelector('input[name="city"]').value;
+    const post = document.querySelector('input[name="post"]').value;
+    const Address = `${NoAddr} ตำบล${subdistrict} อำเภอ${district} จังหวัด${city} ไปรษณีย์ ${post}`;
+
+    try {
+      const response = await Axios.post(
+        "http://localhost:5000/insertDataAccount",
+        {
+          telephone: tel,
+          address: Address,
+          firstname: firstName,
+          lastname: lastName,
+          username: dataUser.username,
+        }
+      );
+      if (response.status === 200) {
+        alert("เสร็จเรียบร้อย");
+      } else {
+        console.error("error");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("มีผู้ใช้ Email หรือ Username นี้แล้ว");
+    }
+
+    console.log(Address);
+  };
+
+  const clearInputField = (inputName) => {
+    const inputElement = document.querySelector(`input[name="${inputName}"]`);
+    if (inputElement) {
+      inputElement.value = ""; // ล้างค่าของช่อง input
+    }
+  };
+
+  const clearInputFields = () => {
+    clearInputField("firstName");
+    clearInputField("lastName");
+    clearInputField("tel");
+    clearInputField("NoAddr");
+    clearInputField("subdistrict");
+    clearInputField("district");
+    clearInputField("city");
+    clearInputField("post");
+  };
+
+  return (
     <div style={{ height: "89vh" }}>
       {/* <button onClick={getData}>test</button> */}
       <div style={{ padding: "0px 25px", paddingTop: "20px" }}>
-        <div style={{ marginBottom: "20px", borderBottom: "2px solid #ECEDED", paddingBottom: "20px", display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+        <div
+          style={{
+            marginBottom: "20px",
+            borderBottom: "2px solid #ECEDED",
+            paddingBottom: "20px",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
           <h1 style={{ color: "#000", fontSize: "30px", fontWeight: "600" }}>
             Account Settings
           </h1>
@@ -24,19 +91,153 @@ const SettingsContent = ({ dataUser, setDataUser }) => {
         </div>
         <div style={{ overflow: "auto" }}>
           <label htmlFor="firstName">First name : </label>
-          <input placeholder={null} type="text" className="input" required="" name='firstName' style={{ marginRight: "40px" }} />
+          <input
+            placeholder={dataUser.firstname}
+            type="text"
+            className="input"
+            required=""
+            name="firstName"
+            style={{ marginRight: "40px" }}
+          />
           <label htmlFor="lastName">Last name : </label>
-          <input placeholder={null} type="text" className="input" required="" name='lastName' style={{ marginBottom: "10px" }} /> <br />
-
+          <input
+            placeholder={dataUser.lastname}
+            type="text"
+            className="input"
+            required=""
+            name="lastName"
+            style={{ marginBottom: "10px" }}
+          />
+          <br />
           <label htmlFor="tel">Telephone Number : </label>
-          <input placeholder={null} type="text" className="input" required="" name='tel' style={{ marginRight: "40px", marginBottom: "10px" }} /> <br />
+          <input
+            placeholder={dataUser.telephone}
+            type="text"
+            className="input"
+            required=""
+            name="tel"
+            style={{ marginRight: "40px", marginBottom: "10px" }}
+          />
+          <br />
+          <div style={{ marginTop: "30px" }}>
+            <h2 style={{ marginBottom: "20px" }}>Address</h2>
+            <label
+              htmlFor="NoAddr"
+              style={{ fontFamily: "'IBM Plex Sans Thai', sans-serif" }}
+            >
+              บ้านเลขที่ :
+            </label>
+            <input
+              placeholder={null}
+              type="text"
+              className="input"
+              required=""
+              name="NoAddr"
+              style={{ marginRight: "40px", marginBottom: "10px" }}
+            />
+            <label
+              htmlFor="subdistrict"
+              style={{ fontFamily: "'IBM Plex Sans Thai', sans-serif" }}
+            >
+              ตำบล :
+            </label>
+            <input
+              placeholder={null}
+              type="text"
+              className="input"
+              required=""
+              name="subdistrict"
+              style={{ marginRight: "40px", marginBottom: "10px" }}
+            />
+            <label
+              htmlFor="district"
+              style={{ fontFamily: "'IBM Plex Sans Thai', sans-serif" }}
+            >
+              อำเภอ :
+            </label>
+            <input
+              placeholder={null}
+              type="text"
+              className="input"
+              required=""
+              name="district"
+              style={{ marginRight: "40px", marginBottom: "10px" }}
+            />
+            <br />
+            <label
+              htmlFor="city"
+              style={{ fontFamily: "'IBM Plex Sans Thai', sans-serif" }}
+            >
+              จังหวัด :
+            </label>
+            <input
+              placeholder={null}
+              type="text"
+              className="input"
+              required=""
+              name="city"
+              style={{ marginRight: "40px", marginBottom: "10px" }}
+            />
+            <label
+              htmlFor="post"
+              style={{ fontFamily: "'IBM Plex Sans Thai', sans-serif" }}
+            >
+              ไปรษณีย์ :
+            </label>
+            <input
+              placeholder={null}
+              type="text"
+              className="input"
+              required=""
+              name="post"
+              style={{ marginRight: "40px", marginBottom: "10px" }}
+            />
+          </div>
+        </div>
 
-          <label htmlFor="address">Address : </label>
-          <input placeholder={null} type="text" className="input" required="" name='address' style={{ marginRight: "40px", marginBottom: "10px", width: "500px" }} />
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "flex-end",
+            marginTop: "50px",
+          }}
+        >
+          <button
+            type="submit"
+            onClick={handleSubmit}
+            style={{
+              padding: "10px 18px",
+              fontSize: "18px",
+              borderRadius: "7px",
+              background: "#4A5C2F",
+              color: "white",
+              border: "none",
+              cursor: "pointer",
+            }}
+          >
+            Submit
+          </button>
+          <button
+            type="button"
+            onClick={clearInputFields}
+            style={{
+              padding: "10px 18px",
+              fontSize: "18px",
+              borderRadius: "7px",
+              background: "#4A5C2F",
+              color: "white",
+              border: "none",
+              marginLeft: "20px",
+              cursor: "pointer",
+            }}
+          >
+            Clear
+          </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SettingsContent
+export default SettingsContent;
